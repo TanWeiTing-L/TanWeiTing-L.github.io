eb9cef66560db415b3c89cd9fbc0f9ad59e000f9
Monti <89173326+TanWeiTing-L@users.noreply.github.com>
2024-12-19 21:22:35 +0800

    Added loading to newly visible DOM elements

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const isElementVisible = (ele) => {
-    const rect = ele.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-};
+const targets = document.querySelectorAll('.mainContent div');
+
+if (targets.length > 0) {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const target = entry.target; // The observed element
+
+      if (entry.isIntersecting) {
+        target.classList.add("loaded");
+      } else {
+      }
+    });
+  });
+
+  // Observe each matching element
+  targets.forEach((target) => observer.observe(target));
+} else {
+  console.log('No matching elements found in the DOM');
+}
