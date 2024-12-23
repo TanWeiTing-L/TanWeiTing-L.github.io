f38f6b0ef54ba1c11b4709b5996c502e8118bef4
Monti <89173326+TanWeiTing-L@users.noreply.github.com>
2024-12-23 10:54:18 +0800

    Reformatting, added password generator

--- script.js
+++ script.js
@@ -27,3 +27,35 @@ function feedbackSubmit() {
   emailInput.value = '';
   messageInput.value = '';
   }
+
+function generatePassword() {
+    let characters = ' ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    if (document.getElementById('specialCharacters').checked) {
+        characters += '!@#$%^&*()_+';
+    }
+    let password = '';
+    const length = 12; // Desired password length
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+
+    document.getElementById('password').innerText = password;
+}
+
+
+// copy to clipboard
+function copyToClipboard() {
+    const password = document.getElementById('password').innerText;
+    navigator.clipboard.writeText(password).then(() => {
+        console.log('Password copied to clipboard');
+        // set the element to "Success" for 2 seconds
+        document.getElementById('copy').innerText = 'Copied!';
+        setTimeout(() => {
+            document.getElementById('copy').innerText = 'Copy to clipboard';
+        },
+        1000);
+
+    });
+}
